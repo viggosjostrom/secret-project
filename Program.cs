0a4679f168da6709b02eebd11ee759d6b8eb7672
viggosjostrom <viggosjostrom1@gmail.com>
2023-10-04 10:26:57 +0200

    lagt till variabler

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hej hej igen");
+
+
+int num = 0;
+string name = "viggo";
+Console.WriteLine($"Mitt namn är {name}"); 
